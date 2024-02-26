fa5897db9506de94462ff2720cc8d940270594a6
Matt Hibbs <mhibbs@trinity.edu>
2024-02-26 14:26:06 -0600

    More graphics stuff

--- app/src/main/java/cs2/graphics/GraphicsStuff.java
+++ app/src/main/java/cs2/graphics/GraphicsStuff.java
@@ -6,11 +6,14 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
 public class GraphicsStuff extends Application {
+
+
   public void start(Stage primaryStage) {
-    Canvas canva = new Canvas(600,400);
+    Canvas canva = new Canvas(600,600);
     Scene sc = new Scene(new StackPane(canva));
     primaryStage.setScene(sc);
     primaryStage.show();
@@ -25,5 +28,48 @@ public class GraphicsStuff extends Application {
     g.setStroke(Color.ORCHID);
     g.strokeLine(0,400, 600,0);
 
+    Color bodyColor = Color.rgb(0,0,100);
+
+    g.setFill(Color.WHITE);
+    g.fillRect(0,0, canva.getWidth(),canva.getHeight());
+    g.setFill(bodyColor);
+    g.fillRect(200,200, 200,200);
+    g.fillOval(200,100, 200,200);
+    g.fillOval(200,300, 200,200);
+
+    g.setFill(Color.rgb(237, 210, 135));
+    g.fillOval(200,150, 100,100);
+    g.fillOval(300,150, 100,100);
+    g.fillArc(200,100, 200,200, 180,180, ArcType.ROUND);
+
+    double[] xs = {300, 320, 300, 280};
+    double[] ys = {200, 220, 240, 220};
+    g.setFill(bodyColor);
+    g.fillPolygon(xs, ys, 4);
+
+    g.setLineWidth(2);
+    for(int x=0; x<canva.getWidth(); x++) {
+      g.setStroke(Color.rgb(x * 255 / 600,0,0));
+      g.strokeLine(x,0, x,canva.getHeight());
+    }
+
+    g.setFill(Color.WHITE);
+    g.fillRect(0,0, canva.getWidth(),canva.getHeight());
+    g.setStroke(Color.BLACK);
+    g.setLineWidth(1);
+
+    drawCircle(g, 300,300,150);
+
   }
+
+  public void drawCircle(GraphicsContext g, double x, double y, double r) {
+    g.strokeOval(x-r,y-r, r*2,r*2);
+    if(r > 5) {
+      drawCircle(g, x-r, y, r/2);
+      drawCircle(g, x+r, y, r/2);
+      drawCircle(g, x, y-r, r/2);
+      drawCircle(g, x, y+r, r/2);
+    }
+  }
+
 }
