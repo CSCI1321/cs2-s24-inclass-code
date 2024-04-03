9c4f40f23f4dde16ef233f454cfd5fe09fd17668
Matt Hibbs <mhibbs@trinity.edu>
2024-04-03 13:22:49 -0500

    Finished map stuff

--- app/src/main/java/cs2/util/MapStuff.java
+++ app/src/main/java/cs2/util/MapStuff.java
@@ -3,6 +3,7 @@ package cs2.util;
 import java.util.*;
 
 public class MapStuff {
+
   public static void main(String[] args) {
     
     HashMap<Character,String> map = new HashMap<Character,String>();
@@ -10,6 +11,26 @@ public class MapStuff {
     map.put('C',"Carrot");
     map.put('B',"banana");
     System.out.println(map);
+
+    System.out.println(map.containsKey('Z'));
+    System.out.println(map.containsKey('C'));
+    System.out.println(map.containsValue("Carrot"));
+    System.out.println(map.containsValue("carrot"));
+    System.out.println(map.size());
+    System.out.println(map.get('A'));
+    System.out.println(map.get('Q'));
+    System.out.println(map.remove('B'));
+    System.out.println(map);
+    System.out.println(map.keySet());
+
+    for(Character c : map.keySet()) {
+      System.out.println(c + " is " + map.get(c));
+    }
+
+    for(String s : map.values()) {
+      System.out.println(s);
+    }
+
 
 
 
--- app/src/main/java/cs2/util/TextAnalysis.java
+++ app/src/main/java/cs2/util/TextAnalysis.java
@@ -25,6 +25,39 @@ public class TextAnalysis {
     System.out.println(words.size());
     return words;
   }
+
+  public static HashMap<String, Integer> getWordCounts(String filename) {
+    HashMap<String, Integer> myMap = new HashMap<String, Integer>();
+    try {
+      File f = new File(filename);
+      Scanner fin = new Scanner(f);
+      while(fin.hasNextLine()) {
+        String line = fin.nextLine();
+        String[] parts = line.split("\\s");
+        for(int i=0; i<parts.length; i++) {
+          parts[i] = parts[i].replaceAll("[^A-Za-z]", "");
+          String currWord = parts[i].toLowerCase();
+
+          if(!currWord.equals("")) {
+            if(myMap.containsKey(currWord)) {
+              //myMap[currWord] = myMap[currWord] + 1
+              myMap.put(currWord, myMap.get(currWord) + 1);
+            } else {
+              //myMap[currWord] = 1
+              myMap.put(currWord, 1);
+            }
+          }
+
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println(myMap);
+    return myMap;
+  }
+
+
   public static void main(String[] args) {
     HashSet<String> temp = getWords("tempest.txt");
     HashSet<String> mid = getWords("midsummer.txt");
@@ -41,6 +74,27 @@ public class TextAnalysis {
     for(String myWord : mid) {
       System.out.println(myWord);
     }
+
+    HashMap<String, Integer> tempCounts = getWordCounts("tempest.txt");
+
+    String freqWord = "ZZZZZ";
+    for(String word : tempCounts.keySet()) {
+      Integer count = tempCounts.get(word);
+      if(tempCounts.get(freqWord) == null || count > tempCounts.get(freqWord)) {
+        freqWord = word;
+      }
+    }
+    System.out.println(freqWord + " happens " + tempCounts.get(freqWord) + " times");
+
+    for(Map.Entry<String,Integer> entry : tempCounts.entrySet()) {
+      if(entry.getValue() > 100) {
+        System.out.println(entry.getKey());
+      }
+    }
+    
+
+
+
   }
 
 
