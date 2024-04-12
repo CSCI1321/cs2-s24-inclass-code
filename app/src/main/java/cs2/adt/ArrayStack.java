b0844f66871d28b9fe35b6c2a0d64f2432d04d2b
Matt Hibbs <mhibbs@trinity.edu>
2024-04-12 13:26:35 -0500

    Finished array stack, started tester code

--- app/src/main/java/cs2/adt/ArrayStack.java
+++ app/src/main/java/cs2/adt/ArrayStack.java
@@ -1,5 +1,7 @@
 package cs2.adt;
 
+import java.util.EmptyStackException;
+
 public class ArrayStack<T> implements Stack<T> {
   private T[] arr;
   private int len;
@@ -10,14 +12,32 @@ public class ArrayStack<T> implements Stack<T> {
   }
 
   public void push(T item) {
+    if(len == arr.length) {
+      T[] tmp = (T[])new Object[arr.length * 2];
+      for(int i=0; i<arr.length; i++) {
+        tmp[i] = arr[i];
+      }
+      arr = tmp;
+    }
     arr[len] = item;
     len += 1;
   }
   public T pop() {
+    if(isEmpty()) {
+      throw new EmptyStackException();
+    }
     len -= 1;
     return arr[len];
   }
-
+  public T peek() {
+    if(isEmpty()) {
+      throw new EmptyStackException();
+    }
+    return arr[len - 1];
+  }
+  public boolean isEmpty() {
+    return len == 0;
+  }
 
   /* Original push & pop
   public void push(T item) {
@@ -37,10 +57,5 @@ public class ArrayStack<T> implements Stack<T> {
     arr = tmp;
     return toReturn;
   } */
-  public T peek() {
-    return arr[arr.length - 1];
-  }
-  public boolean isEmpty() {
-    return arr.length == 0;
-  }
+
 }
---
+++ app/src/test/java/cs2/adt/StackTester.java
@@ -0,0 +1,24 @@
+package cs2.adt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class StackTester {
+  Stack<Integer> s;
+  @BeforeEach
+  void init() {
+    s = new ArrayStack<Integer>();
+  }
+  @Test
+  void testPushPop() {
+    assertTrue(s.isEmpty());
+    s.push(7);
+    assertFalse(s.isEmpty());
+    assertTrue(s.peek() == 7);
+    assertTrue(s.pop() == 7);
+    assertTrue(s.isEmpty());
+    s.pop();
+  }
+}
